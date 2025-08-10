85da55f2a6de652267b9698f836958d32cb25863
BikoMaster6589 <bikomaster9459@gmail.com>
2025-08-10 12:40:18 +0530

    db

--- db.js
+++ db.js
@@ -1,13 +1,26 @@
-const {Pool} = require('pg');
+// const {Pool} = require('pg');
 
-const db = new Pool({
-    user : 'postgres',
-    database : 'Tutorial_Db',
-    password : 'mohan',
-    host : 'localhost',
-    port : 5432
+// const db = new Pool({
+//     user : 'postgres',
+//     database : 'Tutorial_Db',
+//     password : 'mohan',
+//     host : 'localhost',
+//     port : 5432
+// });
+
+// db.connect();
+
+
+
+import { Pool } from 'pg';
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
-db.connect();
 
-module.exports = db;
+
+module.exports = pool;
